b323b770527111ebba00bb89422d867343f5955e
Chris Gomez <cagomez@cellbycell.com>
2026-01-07 08:52:44 -0500

    Completed demo through creating agent.
    Agent had no instructions.
    Agent has no knowledge.
    Agent is created using C# and .NET 10
    Agent uses Persistent Agents package.
    
    Completed pizzabot demo through creating the agent
    
    Agent had no instructions.
    Agent has no knowledge.
    Agent is created using C# and .NET 10
    Agent uses Persistent Agents package.

---
+++ workshop/pizzaagent/Program.cs
@@ -0,0 +1,90 @@
+﻿using Azure.AI.Agents.Persistent;
+using Azure.AI.Projects;
+using Azure.Identity;
+
+// Create the Foundry Project Client
+AIProjectClient projectClient = new AIProjectClient(
+    new Uri("<your-foundry-endpoint>"),
+    new DefaultAzureCredential()
+);
+
+// Get the Persistent Agents Client
+PersistentAgentsClient agentsClient = projectClient.GetPersistentAgentsClient();
+
+// Create an Agent
+PersistentAgent agent = agentsClient.Administration.CreateAgent(
+    model: "gpt-4o",
+    name: "pizza-agent"
+);
+
+Console.WriteLine($"Created agent with ID: {agent.Id}");
+
+// Create a Thread for the Agent
+PersistentAgentThread thread = agentsClient.Threads.CreateThread();
+Console.WriteLine($"Created thread with ID: {thread.Id}");
+
+// Manage the conversation with the Agent
+try
+{
+    string[] exitCommands = ["exit", "quit"];
+
+    while (true)
+    {
+        Console.Write("You: ");
+        string? userInput = Console.ReadLine();
+
+        // loop if no input
+        if (string.IsNullOrWhiteSpace(userInput))
+        {
+            continue;
+        }
+
+        // break out if exit commands are given
+        if (exitCommands.Contains(userInput.ToLower()))
+        {
+            break;
+        }
+
+        // send user input to agent thread
+        PersistentThreadMessage message = agentsClient.Messages.CreateMessage(
+            threadId: thread.Id,
+            role: MessageRole.User,
+            content: userInput
+            );
+
+        // run agent
+        ThreadRun run = agentsClient.Runs.CreateRun(
+            agent: agent,
+            thread: thread
+            );        
+
+        // wait for run to complete
+        do
+        {
+            Thread.Sleep(500);
+            run = agentsClient.Runs.GetRun(thread.Id, run.Id);
+        }
+        while (run.Status == RunStatus.Queued || run.Status == RunStatus.InProgress);
+
+        // the agent had run, get the last message from the thread
+        var messages = agentsClient.Messages.GetMessages(
+            threadId: thread.Id,
+            order: ListSortOrder.Descending
+        );
+
+        MessageTextContent? first_message = messages.FirstOrDefault()?.ContentItems
+                                            .OfType<MessageTextContent>()
+                                            .FirstOrDefault();
+
+        if (first_message != null)
+        {
+            Console.WriteLine($"Agent: {first_message.Text}");
+        }                                    
+    }
+}
+finally
+{
+    string agentToBeDeletedId = agent.Id;
+    bool deleted = agentsClient.Administration.DeleteAgent(agent.Id);
+    Console.WriteLine($"Attempting to delete agent with ID: {agentToBeDeletedId}, success: {deleted}");
+}
